9bf49d44d68142cf50e0ec8ab63b4fc79f419826
chinhis <chinhis21@gmail.com>
2022-04-12 18:05:17 +0300

    little refactoring (added abstract class)

--- Vendor/Auth/Auth.php
+++ Vendor/Auth/Auth.php
@@ -9,29 +9,25 @@
  */
 namespace Vendor\Auth;
 
-interface authFunctions {
+interface authFunctions 
+{
     public function logOut($login);
+    public function createAuth();
 }
 
-class Auth{
+abstract class BaseAuthMethods
+{
     const salt=159753;
-    private $login;
-    private $password;
-    private $ip;
+    protected $login;
+    protected $password;
+    protected $ip;
     
     function __construct($login,$password,$ip){
         $this->login=$login;
         $this->password=$password;
         $this->ip=$ip;
     }
-    
-    public function createAuth(){
-        session_start();
-        $_SESSION['login']=$this->login;
-        $_SESSION['password']=md5($this->password.self::salt);
-        $_SESSION['ip']=$this->ip;
-    }
-    
+        
     public function checkAuth($login,$password,$ip,$salt){
         $passwordHash=md5($password.$salt);
         if($_SESSION['login']==$login && $_SESSION['password']==$passwordHash && $_SESSION['ip']==$ip){
@@ -40,12 +36,24 @@ class Auth{
         else{
             return false;
         }
-    }
-    
+    }   
 }
 
-
-class FinalyAuth extends Auth implements authFunctions{
+class Auth extends BaseAuthMethods implements authFunctions
+{
+    public function createAuth(){
+        session_start();
+        $_SESSION['login']=$this->login;
+        $_SESSION['password']=md5($this->password.self::salt);
+        $_SESSION['ip']=$this->ip;
+        if(self::checkAuth($this->login,$this->password,$this->ip,self::salt)){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
     public function logOut($login){
         if($_SESSION['login']==$login){
             unset($_SESSION['login']);
@@ -60,17 +68,24 @@ class FinalyAuth extends Auth implements authFunctions{
     }
 }
 
-$a=new FinalyAuth('admin','0000','192.168.1.11');
-$a->createAuth();
+$createLoginSession = new Auth('admin','0000','192.168.1.11');
+$loginStatus = $createLoginSession -> createAuth();
 
-if($a->checkAuth('admin','0000','192.168.1.11','159753')===true){
+if($loginStatus===true){ //show message if user logined
     echo 'logined'."\r\n";
 }
 else {
     echo 'NOT logined'."\r\n";
 }
 
-if($a->logOut('admin1')===true){
+if(Auth::checkAuth('admin','0000','192.168.1.11','hgjghgf')===true){ //check random user if logined in system
+    echo 'logined'."\r\n";
+}
+else {
+    echo 'NOT logined'."\r\n";
+}
+
+if(Auth::logOut('admin1')===true){
     echo 'logout DONE'."\r\n";
 }
 else {
