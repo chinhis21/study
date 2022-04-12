d209b69d258efc839f889ec87df25297ade15757
chinhis <chinhis21@gmail.com>
2022-04-12 17:27:09 +0300

    added child class and intrface

--- Vendor/Auth/Auth.php
+++ Vendor/Auth/Auth.php
@@ -9,11 +9,15 @@
  */
 namespace Vendor\Auth;
 
+interface authFunctions {
+    public function logOut($login);
+}
+
 class Auth{
     const salt=159753;
     private $login;
     private $password;
-    private $ip='127.0.0.1';
+    private $ip;
     
     function __construct($login,$password,$ip){
         $this->login=$login;
@@ -40,12 +44,35 @@ class Auth{
     
 }
 
-$a=new Auth('admin','0000','192.168.1.11');
+
+class FinalyAuth extends Auth implements authFunctions{
+    public function logOut($login){
+        if($_SESSION['login']==$login){
+            unset($_SESSION['login']);
+            unset($_SESSION['password']);
+            unset($_SESSION['ip']);
+            session_destroy();
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
+
+$a=new FinalyAuth('admin','0000','192.168.1.11');
 $a->createAuth();
-$loginStatus = $a->checkAuth('admin','0000','192.168.1.11','159753');
-if($loginStatus===true){
+
+if($a->checkAuth('admin','0000','192.168.1.11','159753')===true){
     echo 'logined'."\r\n";
 }
 else {
     echo 'NOT logined'."\r\n";
+}
+
+if($a->logOut('admin1')===true){
+    echo 'logout DONE'."\r\n";
+}
+else {
+    echo 'logout FAIL. Check login'."\r\n";
 }
